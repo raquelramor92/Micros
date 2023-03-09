76ce9d2341da9ad942a692bf96e5864fff910dda
raquelramor92 <61478827+raquelramor92@users.noreply.github.com>
2023-03-08 21:20:53 -0600

    Update Tarea2_Conejo_Lopez_Ramirez_Vega.py
    Agregando línea para diferenciar el main

--- Tarea 2/Tarea2_Conejo_Lopez_Ramirez_Vega.py
+++ Tarea 2/Tarea2_Conejo_Lopez_Ramirez_Vega.py
@@ -7,7 +7,7 @@ A = [ ]
 def change_data(array):
 	for i in range (15):
 		A[i]=30
-	print ('______________________Ejecucion de reescritura con 30: primera mitad del arreglo')
+		print ('______________________Ejecucion de reescritura con 30: primera mitad del arreglo')
 	print (A)
 	return A
 #Function: adding 33 units to each numbers in the second
@@ -18,14 +18,16 @@ def add_33(array):
 		A[i]+=33
 	print('______________________Ejecucion de reescritura con suma de 33 unidades')
 	print (A)
-#Creating array, random numbers added
-for i in range (30):
-    A.append(random.randint(1, 100))
-print('______________________Creando arreglo de numeros aleatorios')    
-print (A)
-#Using both functions created in cascade mode
-add_33(change_data(A))
-print("______________________Por favor espere 2 segundos......")
-time.sleep(2)
-print ("---Tiempo de ejecucion")
-print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == "__main__":
+	#Creating array, random numbers added
+	for i in range (30):
+		A.append(random.randint(1, 100))
+	print('______________________Creando arreglo de numeros aleatorios')
+	print (A)
+	#Using both functions created in cascade mode
+	add_33(change_data(A))
+	print("______________________Por favor espere 2 segundos......")
+	time.sleep(2)
+	print ("---Tiempo de ejecucion")
+	print("--- %s seconds ---" % (time.time() - start_time))
