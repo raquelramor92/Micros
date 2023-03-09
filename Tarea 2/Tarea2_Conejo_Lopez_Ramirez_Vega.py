f7b8a479629bf297784a87dd273c619c36455571
raquelramor92 <61478827+raquelramor92@users.noreply.github.com>
2023-03-08 21:04:38 -0600

    Create Tarea2_Conejo_Lopez_Ramirez_Vega.py
    El programa por el momento hace lo solicitado, está sin aplicar paralelismo

---
+++ Tarea 2/Tarea2_Conejo_Lopez_Ramirez_Vega.py
@@ -0,0 +1,31 @@
+import random
+import time
+start_time = time.time()
+#Defining variables 
+A = [ ]
+#Function: changing first 15 numbers for the number 30
+def change_data(array):
+	for i in range (15):
+		A[i]=30
+	print ('______________________Ejecucion de reescritura con 30: primera mitad del arreglo')
+	print (A)
+	return A
+#Function: adding 33 units to each numbers in the second
+#half of the array
+def add_33(array):	
+	for i in range (15):
+		i += 15
+		A[i]+=33
+	print('______________________Ejecucion de reescritura con suma de 33 unidades')
+	print (A)
+#Creating array, random numbers added
+for i in range (30):
+    A.append(random.randint(1, 100))
+print('______________________Creando arreglo de numeros aleatorios')    
+print (A)
+#Using both functions created in cascade mode
+add_33(change_data(A))
+print("______________________Por favor espere 2 segundos......")
+time.sleep(2)
+print ("---Tiempo de ejecucion")
+print("--- %s seconds ---" % (time.time() - start_time))
